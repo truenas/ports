58717fbe3d340ae7be33d87118ee0019759352e1
John Hixson <john@ixsystems.com>
2015-01-31 15:10:39 -0800

    Apparently I fudged the patch.
    Ticket: #7773

--- net/samba41/files/patch-source3__param__loadparm.c
+++ net/samba41/files/patch-source3__param__loadparm.c
@@ -1,14 +1,15 @@
---- ./source3/param/loadparm.c.orig	2015-01-28 00:32:27.759378400 -0800
-+++ ./source3/param/loadparm.c	2015-01-28 00:54:41.125286268 -0800
-@@ -3170,9 +3170,11 @@
- 
- 	/* if it's already been set by the command line, then we don't
- 	   override here */
-+	/*
- 	if (parm_table[parmnum].flags & FLAG_CMDLINE) {
+--- source3/param/loadparm.c.orig	2015-01-28 00:32:27.759378400 -0800
++++ source3/param/loadparm.c	2015-01-31 15:04:49.919907614 -0800
+@@ -3174,10 +3174,12 @@
  		return true;
+ 	}
+ 
++	/*
+ 	if (parm_table[parmnum].flags & FLAG_DEPRECATED) {
+ 		DEBUG(1, ("WARNING: The \"%s\" option is deprecated\n",
+ 			  pszParmName));
  	}
 +	*/
  
- 	if (parm_table[parmnum].flags & FLAG_DEPRECATED) {
- 		DEBUG(1, ("WARNING: The \"%s\" option is deprecated\n",
+ 	/* we might point at a service, the default service or a global */
+ 	if (snum < 0) {
