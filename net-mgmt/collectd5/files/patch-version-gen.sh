b96dd30b7b800c48419f923e9490b73de1e8ad2d
Kris Moore <kris@ixsystems.com>
2017-09-19 13:59:10 -0400

    Restore our heavily customized version of collectd from 2017Q2 branch

--- net-mgmt/collectd5/files/patch-version-gen.sh
+++ net-mgmt/collectd5/files/patch-version-gen.sh
@@ -1,11 +1,11 @@
---- version-gen.sh.orig	2020-03-19 09:52:56.313967100 +0100
-+++ version-gen.sh	2020-03-27 19:15:53.407855000 +0100
+--- version-gen.sh.orig	2017-02-03 20:46:49 UTC
++++ version-gen.sh
 @@ -2,10 +2,6 @@
  
- DEFAULT_VERSION="5.11.0.git"
+ DEFAULT_VERSION="5.7.1.git"
  
 -if [ -d .git ]; then
--	VERSION="`git describe --dirty=+ --abbrev=7 2> /dev/null | sed -e '/^collectd-/!d' -e 's///' -e 'y/-/./'`"
+-	VERSION="`git describe --dirty=+ --abbrev=7 2> /dev/null | grep collectd | sed -e 's/^collectd-//' -e 's/-/./g'`"
 -fi
 -
  if test -z "$VERSION"; then
