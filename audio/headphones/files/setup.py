251acd3531b6ec3f11584e616ccf067023767d06
ZackaryWelch <welch.zackary@gmail.com>
2018-05-22 13:17:28 -0400

    Initial commit for Headphones port

---
+++ audio/headphones/files/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import os
+from distutils.core import setup
+
+setup(
+    name='Headphones',
+    version='0.5.19',
+    description='Automated music downloader for NZB and Torrent',
+    url='https://github.com/rembo10/headphones'
+)
