ed1d005eb8a2f3251b019e10dc3fe74c665b0f6a
ZackaryWelch <welch.zackary@gmail.com>
2018-05-22 15:13:12 -0400

    Initial commit for HTPC-Manager port

---
+++ multimedia/htpc-manager/files/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import os
+from distutils.core import setup
+
+setup(
+    name='HTPC-Manager',
+    version='0.11.0',
+    description='Python based web application to manage the software on an HTPC',
+    url='https://github.com/styxit/HTPC-Manager'
+)
