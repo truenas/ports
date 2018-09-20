a443c2d3efb8b84e14800e1ec56c4b65e0345881
Ken Moore <ken@ixsystems.com>
2018-09-20 10:53:57 -0400

    Delete patch-examples_alffplay.cpp
    Delete obsolete patch file for audio/openal-soft
    This should make the TrueOS version of the port exactly match the FreeBSD version now.

--- audio/openal-soft/files/patch-examples_alffplay.cpp
+++
@@ -1,9 +0,0 @@
---- examples/alffplay.cpp.orig 2018-02-19 01:00:00 UTC
-+++ examples/alffplay.cpp
-@@ -18,6 +18,7 @@
- #include <mutex>
- #include <deque>
- #include <array>
-+#include <cmath>
- 
- extern "C" {
