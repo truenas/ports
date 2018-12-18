1d875fd56dda44b322765901ae1a2c65378ee88f
Kris Moore <kris@ixsystems.com>
2018-12-18 12:22:10 -0500

    Make python 3.7 the default

--- Mk/bsd.default-versions.mk
+++ Mk/bsd.default-versions.mk
@@ -85,7 +85,7 @@ PYTHON_DEFAULT?=	2.7
 # Possible values: 2.7
 PYTHON2_DEFAULT?=	2.7
 # Possible values: 3.4, 3.5, 3.6, 3.7
-PYTHON3_DEFAULT?=	3.6
+PYTHON3_DEFAULT?=	3.7
 # Possible values: 2.3, 2.4, 2.5
 RUBY_DEFAULT?=		2.4
 # Possible values: 4.6, 4.7, 4.8
