dd6817cec32bd606af2d1f61ca4f3b7e5a870eab
themylogin <themylogin@gmail.com>
2019-01-25 22:52:21 +0100

    fix(net-mgmt/collectd5): Make rrdcached plugin report rrdc_update failures as warnings (as rrdtool plugin does)
    (cherry picked from commit b49f694ba34a5a75f30d8e4ae125039ddf9a225b)

---
+++ net-mgmt/collectd5/files/patch-src__rrdcached.c
@@ -0,0 +1,13 @@
+--- src/rrdcached.c.orig	2017-06-06 20:13:54.693164693 +0200
++++ src/rrdcached.c	2019-01-25 22:45:29.468646783 +0100
+@@ -463,8 +463,8 @@
+       /* else: report the error and fail */
+     }
+ 
+-    ERROR("rrdcached plugin: rrdc_update (%s, [%s], 1) failed: %s (status=%i)",
+-          filename, values_array[0], rrd_get_error(), status);
++    WARNING("rrdcached plugin: rrdc_update (%s, [%s], 1) failed: %s (status=%i)",
++            filename, values_array[0], rrd_get_error(), status);
+     return (-1);
+   }
+ 
