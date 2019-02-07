e5af306a09a119c40f9f863ea70204b307ed3357
awalker <awalker@ixsystems.com>
2019-02-07 12:10:34 -0500

    Multiple fixes in vfs_recycle in samba49 port - acl_free() where needed in vfs_recycle - check return value of acl_strip_np - improve logging to catch errnos. - clarify function names
    (cherry picked from commit 511d9a3da1a89fe16b21bb22207a042b4241c8ac)

--- net/samba49/files/patch-source3__modules__vfs_recycle.c
+++ net/samba49/files/patch-source3__modules__vfs_recycle.c
@@ -1,5 +1,5 @@
---- source3/modules/vfs_recycle.c.orig	2018-07-12 04:23:36 UTC 
-+++ source3/modules/vfs_recycle.c
+--- source3/modules/vfs_recycle.c.orig	2018-07-12 04:23:36.000000000 -0400
++++ source3/modules/vfs_recycle.c	2019-02-07 11:54:36.021712000 -0500
 @@ -27,6 +27,7 @@
  #include "system/filesys.h"
  #include "../librpc/gen_ndr/ndr_netlogon.h"
@@ -8,11 +8,11 @@
  
  #define ALLOC_CHECK(ptr, label) do { if ((ptr) == NULL) { DEBUG(0, ("recycle.bin: out of memory!\n")); errno = ENOMEM; goto label; } } while(0)
  
-@@ -38,6 +39,38 @@ static int vfs_recycle_debug_level = DBG
+@@ -38,6 +39,47 @@ static int vfs_recycle_debug_level = DBG
  static int recycle_unlink(vfs_handle_struct *handle,
  			  const struct smb_filename *smb_fname);
  
-+static bool set_root_directory_acl(vfs_handle_struct *handle, const struct smb_filename *smb_fname, mode_t mode)
++static bool set_recycle_acl(vfs_handle_struct *handle, const struct smb_filename *smb_fname, mode_t mode)
 +{
 +	/*
 +	 *	Redmine #73891
@@ -25,13 +25,22 @@
 +	acl_t oldacl, newacl;
 +	oldacl = acl_get_file(smb_fname->base_name, ACL_TYPE_NFS4);
 +	if (oldacl == NULL) {
-+		DBG_ERR("recycle: acl_get_file() failed for %s\n", smb_fname->base_name);
++		DBG_ERR("recycle: acl_get_file() failed for %s: %s\n", 
++			smb_fname->base_name, strerror(errno));
 +		return false; 
 +	}
 +	newacl = acl_strip_np(oldacl, true);
-+	if ( acl_set_file(smb_fname->base_name, ACL_TYPE_NFS4, newacl) < 0 ) {
-+		DBG_ERR("recycle: acl_set_file() failed for %s\n", smb_fname->base_name);
++	if (newacl == NULL) {
++		DBG_ERR("recycle: acl_strip_np() failed for %s: %s\n", 
++			smb_fname->base_name, strerror(errno));
 +		acl_free(oldacl);
++		return false; 
++	}
++	if ( acl_set_file(smb_fname->base_name, ACL_TYPE_NFS4, newacl) < 0 ) {
++		DBG_ERR("recycle: acl_set_file() failed for %s: %s\n",
++			smb_fname->base_name, strerror(errno));
++		acl_free(oldacl);
++		acl_free(newacl);
 +		return false;
 +	}
 +	acl_free(oldacl);
@@ -47,21 +56,21 @@
  static const char *recycle_repository(vfs_handle_struct *handle)
  {
  	const char *tmp_str = NULL;
-@@ -267,6 +300,7 @@ static bool recycle_create_dir(vfs_handl
+@@ -267,6 +309,7 @@ static bool recycle_create_dir(vfs_handl
  	char *tok_str;
  	bool ret = False;
  	char *saveptr;
-+	bool root_acl_set = False;
++	bool recycle_acl_is_set = False;
  
  	mode = recycle_directory_mode(handle);
  
-@@ -313,11 +347,18 @@ static bool recycle_create_dir(vfs_handl
+@@ -313,11 +356,18 @@ static bool recycle_create_dir(vfs_handl
  				ret = False;
  				goto done;
  			}
-+			if (!root_acl_set) {
++			if (!recycle_acl_is_set) {
 +				if ( !lp_parm_bool(SNUM(handle->conn), "recycle", "preserveacl", False)
-+				     && !set_root_directory_acl(handle, smb_fname, mode) ) {
++				     && !set_recycle_acl(handle, smb_fname, mode) ) {
 +					DBG_ERR("recycle: failed to prep ACL on %s\n", smb_fname->base_name);
 +				} 
 +			}
@@ -70,7 +79,7 @@
  		if (strlcat(new_dir, "/", len+1) >= len+1) {
  			goto done;
  		}
-+		root_acl_set = True;
++		recycle_acl_is_set = True;
  		mode = recycle_subdir_mode(handle);
  	}
  
