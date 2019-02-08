1f156693b2dd0792f0a2ac6bae3e732cecf909b4
themylogin <themylogin@gmail.com>
2019-02-08 19:15:52 +0100

    Upgrade collectd to upstream

---
+++ net-mgmt/collectd5/files/patch-src_zfs__arc.c
@@ -0,0 +1,30 @@
+--- src/zfs_arc.c
++++ src/zfs_arc.c
+@@ -197,6 +197,9 @@ static void za_submit_ratio(const char *type_instance, gauge_t hits,
+   za_submit_gauge("cache_ratio", type_instance, ratio);
+ }
+ 
++static value_to_rate_state_t arc_hits_state, arc_misses_state, l2_hits_state,
++    l2_misses_state;
++
+ static int za_read(void) {
+   gauge_t arc_hits, arc_misses, l2_hits, l2_misses;
+   kstat_t *ksp = NULL;
+@@ -278,7 +281,7 @@ static int za_read(void) {
+    * Stop trying if we ever fail to read it, so we don't spam the log.
+    */
+   static int l2_size_avail = 1;
+-  if (l2_size_avail && za_read_gauge(ksp, "l2_size", "cache_size", "L2") != 0)
++  if (l2_size_avail && za_read_gauge(ksp, "l2_asize", "cache_size", "L2") != 0)
+     l2_size_avail = 0;
+ 
+   /* Operations */
+@@ -301,8 +301,6 @@ static int za_read(void) {
+   za_read_derive(ksp, "prefetch_metadata_hits", "cache_result",
+                  "prefetch_metadata-hit");
+   za_read_derive(ksp, "demand_data_misses", "cache_result", "demand_data-miss");
+-  za_read_derive(ksp, "demand_metadata_misses", "cache_result",
+-                 "demand_metadata-miss");
+   za_read_derive(ksp, "prefetch_data_misses", "cache_result",
+                  "prefetch_data-miss");
+   za_read_derive(ksp, "prefetch_metadata_misses", "cache_result",
