5a0f8c56740775f0479ebb996b684459e5037421
Ken Moore <ken@ixsystems.com>
2019-02-21 09:01:14 -0500

    dns/py-pybonjour: Resolve a merge conflict in the patch file. Use the upstream FreeBSD version of the file completely.

--- dns/py-pybonjour/files/patch-pybonjour.py
+++ dns/py-pybonjour/files/patch-pybonjour.py
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
---- pybonjour.py.orig	2017-12-05 12:29:04.005301000 -0500
-+++ pybonjour.py	2017-12-05 12:29:21.721981000 -0500
-@@ -1116,7 +1116,7 @@
-=======
 --- pybonjour.py.orig	2013-10-02 17:41:05 UTC
 +++ pybonjour.py
 @@ -1116,7 +1116,7 @@ def DNSServiceRegister(
->>>>>>> freebsd/master
              type=type(txtRecord)))
  
      if not txtRecord:
