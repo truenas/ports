63b1be0e73b2f528652194f25f21e4886e797b70
Andrew <awalker@ixsystems.com>
2019-03-16 06:28:57 -0400

    tkt-74461: Initial Samba 4.10 port (#231)
    * Initial Samba 4.10 port

---
+++ net/samba410/files/patch-source3__modules__vfs_zfsacl.c
@@ -0,0 +1,40 @@
+--- source3/modules/vfs_zfsacl.c.orig	2018-07-12 08:23:36 UTC
++++ source3/modules/vfs_zfsacl.c
+@@ -51,6 +51,7 @@ static NTSTATUS zfs_get_nt_acl_common(st
+ 	SMB_STRUCT_STAT sbuf;
+ 	const SMB_STRUCT_STAT *psbuf = NULL;
+ 	int ret;
++	bool inherited_is_present = False;
+ 	bool is_dir;
+ 
+ 	if (VALID_STAT(smb_fname->st)) {
+@@ -113,10 +114,12 @@ static NTSTATUS zfs_get_nt_acl_common(st
+ 			aceprop.aceMask |= SMB_ACE4_SYNCHRONIZE;
+ 		}
+ 
+-		if (is_dir && (aceprop.aceMask & SMB_ACE4_ADD_FILE)) {
+-			aceprop.aceMask |= SMB_ACE4_DELETE_CHILD;
+-		}
+ 
++#ifdef ACE_INHERITED_ACE
++ 		if(aceprop.aceFlags & ACE_INHERITED_ACE) {
++ 			inherited_is_present = true;
++ 		}
++#endif
+ 		if(aceprop.aceFlags & ACE_OWNER) {
+ 			aceprop.flags = SMB_ACE4_ID_SPECIAL;
+ 			aceprop.who.special_id = SMB_ACE4_WHO_OWNER;
+@@ -133,6 +136,13 @@ static NTSTATUS zfs_get_nt_acl_common(st
+ 			return NT_STATUS_NO_MEMORY;
+ 	}
+ 
++#ifdef ACE_INHERITED_ACE
++ 	if (!inherited_is_present 
++	    && lp_parm_bool(conn->params->service, "zfsacl", "map_dacl_protected", True)){
++		DBG_DEBUG("setting dacl_protected flag on %s \n", smb_fname->base_name);
++		smbacl4_set_controlflags(pacl, SEC_DESC_DACL_PROTECTED|SEC_DESC_SELF_RELATIVE);
++	}
++#endif
+ 	*ppacl = pacl;
+ 	return NT_STATUS_OK;
+ }
