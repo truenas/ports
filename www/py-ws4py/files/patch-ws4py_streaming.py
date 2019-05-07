c5199dff1d1c0f74ff6051e9f60f23919554cbb7
Waqar Ahmed <waqarsonic1@gmail.com>
2019-05-07 08:52:05 +0500

    Cover utf8 validator usages
    This commit covers utf8 validator usages making sure that the library adheres to the latest changes introduced to utf8 validator giving a significant performance boost.

---
+++ www/py-ws4py/files/patch-ws4py_streaming.py
@@ -0,0 +1,52 @@
+--- ws4py/streaming.py.orig	2019-05-06 23:25:22 UTC
++++ ws4py/streaming.py
+@@ -225,9 +225,9 @@ class Stream(object):
+                         self.message = m
+ 
+                         if some_bytes:
+-                            is_valid, end_on_code_point, _, _ = utf8validator.validate(some_bytes)
++                            is_valid = utf8validator.validate(some_bytes)
+ 
+-                            if not is_valid or (m.completed and not end_on_code_point):
++                            if not is_valid:
+                                 self.errors.append(CloseControlMessage(code=1007, reason='Invalid UTF-8 bytes'))
+                                 break
+ 
+@@ -252,9 +252,9 @@ class Stream(object):
+                         m.completed = (frame.fin == 1)
+                         if m.opcode == OPCODE_TEXT:
+                             if some_bytes:
+-                                is_valid, end_on_code_point, _, _ = utf8validator.validate(some_bytes)
++                                is_valid = utf8validator.validate(some_bytes)
+ 
+-                                if not is_valid or (m.completed and not end_on_code_point):
++                                if not is_valid:
+                                     self.errors.append(CloseControlMessage(code=1007, reason='Invalid UTF-8 bytes'))
+                                     break
+ 
+@@ -280,9 +280,11 @@ class Stream(object):
+                                 elif frame.payload_length > 1:
+                                     reason = some_bytes[2:] if frame.masking_key else frame.body[2:]
+ 
+-                                    if not py3k: reason = bytearray(reason)
+-                                    is_valid, end_on_code_point, _, _ = utf8validator.validate(reason)
+-                                    if not is_valid or not end_on_code_point:
++                                    if not py3k:
++                                        reason = bytearray(reason)
++
++                                    is_valid = utf8validator.validate(reason)
++                                    if not is_valid:
+                                         self.errors.append(CloseControlMessage(code=1007, reason='Invalid UTF-8 bytes'))
+                                         break
+                                     reason = bytes(reason)
+@@ -310,10 +312,4 @@ class Stream(object):
+             frame.body = None
+             frame = None
+ 
+-            if self.message is not None and self.message.completed:
+-                utf8validator.reset()
+-
+-        utf8validator.reset()
+-        utf8validator = None
+-
+         self._cleanup()
