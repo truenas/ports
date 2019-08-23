9ff62a03ebea30eff0ad71787c11fd674edc87c5
Waqar Ahmed <waqarsonic1@gmail.com>
2019-08-23 08:26:55 +0500

    Add VNC Auth support

--- devel/libvirt/files/patch-src_bhyve_bhyve__command.c
+++ devel/libvirt/files/patch-src_bhyve_bhyve__command.c
@@ -1,4 +1,4 @@
---- src/bhyve/bhyve_command.c.orig	2019-08-05 00:05:44 UTC
+--- src/bhyve/bhyve_command.c.orig	2019-04-27 10:31:34 UTC
 +++ src/bhyve/bhyve_command.c
 @@ -236,8 +236,9 @@ bhyveBuildAHCIControllerArgStr(const virDomainDef *def
          goto error;
@@ -22,3 +22,31 @@
                             disk_source);
 
      return 0;
+@@ -383,17 +385,6 @@ bhyveBuildGraphicsArgStr(const virDomainDef *def,
+             goto error;
+         }
+
+-        if (graphics->data.vnc.auth.passwd) {
+-            virReportError(VIR_ERR_CONFIG_UNSUPPORTED, "%s",
+-                           _("vnc password auth not supported"));
+-            goto error;
+-        } else {
+-             /* Bhyve doesn't support VNC Auth yet, so print a warning about
+-              * unauthenticated VNC sessions */
+-             VIR_WARN("%s", _("Security warning: currently VNC auth is not"
+-                              " supported."));
+-        }
+-
+         if (glisten->address) {
+             escapeAddr = strchr(glisten->address, ':') != NULL;
+             if (escapeAddr)
+@@ -415,6 +406,9 @@ bhyveBuildGraphicsArgStr(const virDomainDef *def,
+         }
+
+         virBufferAsprintf(&opt, ":%d", graphics->data.vnc.port);
++        if (graphics->data.vnc.auth.passwd) {
++            virBufferAsprintf(&opt, ",password=%s", graphics->data.vnc.auth.passwd);
++        }
+         break;
+     case VIR_DOMAIN_GRAPHICS_LISTEN_TYPE_SOCKET:
+     case VIR_DOMAIN_GRAPHICS_LISTEN_TYPE_NONE:
