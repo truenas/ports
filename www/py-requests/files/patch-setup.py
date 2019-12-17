c4f2d095147df1540c172a21ce2b18df3b053037
Waqar Ahmed <waqarahmedjoyia@live.com>
2019-12-17 20:36:51 +0500

    Update requests port to accommodate newer urllib3

--- www/py-requests/files/patch-setup.py
+++ www/py-requests/files/patch-setup.py
@@ -1,8 +1,13 @@
-# pytest-cov and its dependencies are not compulsory
-
---- setup.py.orig	2019-01-22 02:52:54 UTC
+--- setup.py.orig	2019-11-29 23:51:26 UTC
 +++ setup.py
-@@ -50,7 +50,6 @@ requires = [
+@@ -44,13 +44,12 @@ packages = ['requests']
+ requires = [
+     'chardet>=3.0.2,<3.1.0',
+     'idna>=2.5,<2.9',
+-    'urllib3>=1.21.1,<1.25',
++    'urllib3>=1.21.1,<1.26',
+     'certifi>=2017.4.17'
+ 
  ]
  test_requirements = [
      'pytest-httpbin==0.0.7',
