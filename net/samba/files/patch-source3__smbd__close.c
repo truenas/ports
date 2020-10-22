41d5f62a70e2575e786c1b6f4d1c0bf87e4a7344
Andrew Walker <awalker@ixsystems.com>
2020-10-22 15:04:13 -0400

    s3:smbd:close - fix deletion of tmp files when steams enabled
    When Adobe Acrobat and possibly other Windows applications generate
    a temporary file, they set a DACL with zero entries in the inital
    CREATE request along with a delete_on_close flag. In samba's close
    function if alternate data stream support is enabled (the default
    in TrueNAS), then delete_all_stream() will fail because it relies
    on several path-based syscalls(). Big picture, vfs_streams_xattr
    needs to provide handle-based alternatives to use for listing and
    reading streams, but this would actually adding new vfs hooks to
    samba as streaminfo can only be retrieved by path as things stand.
    
    For the purpose of this bugfix, simply log when delete_all_streams()
    fails during close, and hope that the streams are implemented as
    xattrs on the file being closed.

--- net/samba/files/patch-source3__smbd__close.c
+++ net/samba/files/patch-source3__smbd__close.c
@@ -1,6 +1,20 @@
---- source3/smbd/close.c.orig	2020-10-08 20:46:24.346293000 -0400
-+++ source3/smbd/close.c	2020-10-08 20:47:34.858038000 -0400
-@@ -666,7 +666,7 @@ static void assert_no_pending_aio(struct files_struct 
+--- work/samba-4.12.7/source3/smbd/close.c.orig	2020-07-02 07:12:21.000000000 -0400
++++ work/samba-4.12.7/source3/smbd/close.c	2020-10-22 14:24:53.857059000 -0400
+@@ -447,8 +447,12 @@ static NTSTATUS close_remove_share_mode(files_struct *
+ 	    && !is_ntfs_stream_smb_fname(fsp->fsp_name)) {
+ 
+ 		status = delete_all_streams(conn, fsp->fsp_name);
++		if (NT_STATUS_EQUAL(status, NT_STATUS_ACCESS_DENIED)) {
++			DEBUG(5, ("delete_all_streams failed with permission"
++				  "error. Continuing with delete\n"));
++		}
+ 
+-		if (!NT_STATUS_IS_OK(status)) {
++		else if (!NT_STATUS_IS_OK(status)) {
+ 			DEBUG(5, ("delete_all_streams failed: %s\n",
+ 				  nt_errstr(status)));
+ 			goto done;
+@@ -666,7 +670,7 @@ static void assert_no_pending_aio(struct files_struct 
  		 * fsp->aio_requests[x], causing a crash.
  		 */
  		while (fsp->num_aio_requests != 0) {
@@ -9,3 +23,17 @@
  		}
  		return;
  	}
+@@ -1204,7 +1208,12 @@ static NTSTATUS close_directory(struct smb_request *re
+ 		    && !is_ntfs_stream_smb_fname(fsp->fsp_name)) {
+ 
+ 			status = delete_all_streams(fsp->conn, fsp->fsp_name);
+-			if (!NT_STATUS_IS_OK(status)) {
++
++			if (NT_STATUS_EQUAL(status, NT_STATUS_ACCESS_DENIED)) {
++				DEBUG(5, ("delete_all_streams failed with permission"
++					  "error. Continuing with delete\n"));
++			}
++			else if (!NT_STATUS_IS_OK(status)) {
+ 				DEBUG(5, ("delete_all_streams failed: %s\n",
+ 					  nt_errstr(status)));
+ 				file_free(req, fsp);
