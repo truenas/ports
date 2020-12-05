6180ac1263186a8f41c30f568dde9c85a96170d9
Andrew Walker <awalker@ixsystems.com>
2020-12-05 13:42:32 -0500

    samba - plumb vss_tval through Samba's VFS
    TrueNAS 12.0 expanded struct smb_filename to include a timeval
    struct vss_tval that is populated during unix_convert() in filename.c.
    This allows simpler checks for whether a file is in a snapshot directory
    (simply checking whether tv_sec > 0), and significantly reduces impact
    of having shadow copies enabled. There were a few places inside
    smbd and vfs modules where synthetic_smb_fname() was called and
    the vss_tval struct wasn't copied into the new smb_filename struct
    resulting in subsequent calls to check_name() / realpath() / stat()
    to fail or give surprising results.
    
    Backport statcache changes from 4.13 to account for VSS timestamps
    in struct smb_filename. Ensure that VSS timestamp is added after
    synthetic_smb_fname() calls where needed.

--- net/samba/files/patch-source3__modules__vfs_fruit.c
+++ net/samba/files/patch-source3__modules__vfs_fruit.c
@@ -1,6 +1,38 @@
---- source3/modules/vfs_fruit.c.orig	2020-08-26 12:43:26.095459000 -0400
-+++ source3/modules/vfs_fruit.c	2020-08-26 12:43:32.741657000 -0400
-@@ -2193,7 +2193,14 @@ static ssize_t fruit_pread_meta_stream(vfs_handle_stru
+--- source3/modules/vfs_fruit.c.orig	2020-04-07 02:45:51.000000000 -0400
++++ source3/modules/vfs_fruit.c	2020-12-07 08:52:21.256596000 -0500
+@@ -895,6 +895,7 @@ static bool readdir_attr_meta_finderi_stream(
+ 	if (stream_name == NULL) {
+ 		return false;
+ 	}
++	stream_name->vss_tval.tv_sec = smb_fname->vss_tval.tv_sec;
+ 
+ 	ret = SMB_VFS_STAT(handle->conn, stream_name);
+ 	if (ret != 0) {
+@@ -1069,6 +1070,7 @@ static uint64_t readdir_attr_rfork_size_stream(
+ 	if (stream_name == NULL) {
+ 		return 0;
+ 	}
++	stream_name->vss_tval.tv_sec = smb_fname->vss_tval.tv_sec;
+ 
+ 	ret = SMB_VFS_STAT(handle->conn, stream_name);
+ 	if (ret != 0) {
+@@ -2021,6 +2023,7 @@ static int fruit_unlink_internal(vfs_handle_struct *ha
+ 	if (rsrc_smb_fname == NULL) {
+ 		return -1;
+ 	}
++	rsrc_smb_fname->vss_tval.tv_sec = smb_fname->vss_tval.tv_sec;
+ 
+ 	rc = fruit_unlink_rsrc(handle, dirfsp, rsrc_smb_fname, true);
+ 	if ((rc != 0) && (errno != ENOENT)) {
+@@ -2131,6 +2134,7 @@ static int fruit_rmdir_internal(struct vfs_handle_stru
+ 			DBG_ERR("synthetic_smb_fname failed\n");
+ 			return -1;
+ 		}
++		ad_smb_fname->vss_tval.tv_sec = smb_fname->vss_tval.tv_sec;
+ 
+ 		/*
+ 		 * Check whether it's a valid AppleDouble file, if
+@@ -2193,7 +2197,14 @@ static ssize_t fruit_pread_meta_stream(vfs_handle_stru
  	int ret;
  
  	nread = SMB_VFS_NEXT_PREAD(handle, fsp, data, n, offset);
@@ -16,3 +48,27 @@
  		return nread;
  	}
  
+@@ -3438,6 +3449,7 @@ static NTSTATUS delete_invalid_meta_stream(
+ 	if (sname == NULL) {
+ 		return NT_STATUS_NO_MEMORY;
+ 	}
++	sname->vss_tval.tv_sec = smb_fname->vss_tval.tv_sec;
+ 
+ 	ret = SMB_VFS_NEXT_UNLINKAT(handle,
+ 			handle->conn->cwd_fsp,
+@@ -4597,6 +4609,7 @@ static void fruit_offload_write_done(struct tevent_req
+ 		if (tevent_req_nomem(src_fname_tmp, req)) {
+ 			return;
+ 		}
++		src_fname_tmp->vss_tval.tv_sec = state->src_fsp->fsp_name->vss_tval.tv_sec;
+ 
+ 		if (is_ntfs_default_stream_smb_fname(src_fname_tmp)) {
+ 			TALLOC_FREE(src_fname_tmp);
+@@ -4613,6 +4626,7 @@ static void fruit_offload_write_done(struct tevent_req
+ 			TALLOC_FREE(src_fname_tmp);
+ 			return;
+ 		}
++		dst_fname_tmp->vss_tval.tv_sec = state->dst_fsp->fsp_name->vss_tval.tv_sec;
+ 
+ 		status = copy_file(req,
+ 				   state->handle->conn,
