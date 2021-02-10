276ca18b66040648b0b1e76ece20af7011c3bcec
themylogin <themylogin@gmail.com>
2021-02-10 09:46:48 +0100

    Collect demand_metadata_misses

--- net-mgmt/collectd5/files/patch-src_zfs__arc.c
+++ net-mgmt/collectd5/files/patch-src_zfs__arc.c
@@ -19,12 +19,3 @@
      l2_size_avail = 0;
  
    /* Operations */
-@@ -322,8 +322,6 @@ static int za_read(void) {
-   za_read_derive(ksp, "prefetch_metadata_hits", "cache_result",
-                  "prefetch_metadata-hit");
-   za_read_derive(ksp, "demand_data_misses", "cache_result", "demand_data-miss");
--  za_read_derive(ksp, "demand_metadata_misses", "cache_result",
--                 "demand_metadata-miss");
-   za_read_derive(ksp, "prefetch_data_misses", "cache_result",
-                  "prefetch_data-miss");
-   za_read_derive(ksp, "prefetch_metadata_misses", "cache_result",
