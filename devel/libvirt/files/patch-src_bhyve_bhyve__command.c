b529683a3ccad53f444d97f80e0b4eaf01d6c58d
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-03-05 22:32:54 +0500

    Update bhyve-src patch

--- devel/libvirt/files/patch-src_bhyve_bhyve__command.c
+++ devel/libvirt/files/patch-src_bhyve_bhyve__command.c
@@ -1,17 +1,6 @@
---- src/bhyve/bhyve_command.c.orig	2020-04-14 22:48:12 UTC
+--- src/bhyve/bhyve_command.c.orig	2021-03-01 10:56:12 UTC
 +++ src/bhyve/bhyve_command.c
-@@ -220,13 +220,20 @@ bhyveBuildAHCIControllerArgStr(const virDomainDef *def
-                            _("unsupported disk device"));
-             goto error;
-         }
-+        if (disk->blockio.logical_block_size) {
-+            virBufferAsprintf(&device, ",sectorsize=%d", disk->blockio.logical_block_size);
-+            if (disk->blockio.physical_block_size) {
-+                virBufferAsprintf(&device, "/%d", disk->blockio.physical_block_size);
-+            }
-+        }
-         virBufferAddBuffer(&buf, &device);
-         virBufferFreeAndReset(&device);
+@@ -217,8 +217,9 @@ bhyveBuildAHCIControllerArgStr(const virDomainDef *def
      }
  
      virCommandAddArg(cmd, "-s");
@@ -21,8 +10,8 @@
 +                           controller->info.addr.pci.function,
                             virBufferCurrentContent(&buf));
  
-     ret = 0;
-@@ -280,6 +287,7 @@ bhyveBuildVirtIODiskArgStr(const virDomainDef *def G_G
+     return 0;
+@@ -269,6 +270,7 @@ bhyveBuildVirtIODiskArgStr(const virDomainDef *def G_G
                             virCommandPtr cmd)
  {
      const char *disk_source;
@@ -30,7 +19,7 @@
  
      if (virDomainDiskTranslateSourcePool(disk) < 0)
          return -1;
-@@ -299,10 +307,17 @@ bhyveBuildVirtIODiskArgStr(const virDomainDef *def G_G
+@@ -288,10 +290,17 @@ bhyveBuildVirtIODiskArgStr(const virDomainDef *def G_G
  
      disk_source = virDomainDiskGetSource(disk);
  
@@ -51,44 +40,7 @@
  
      return 0;
  }
-@@ -366,7 +381,7 @@ static int
- bhyveBuildLPCArgStr(const virDomainDef *def G_GNUC_UNUSED,
-                     virCommandPtr cmd)
- {
--    virCommandAddArgList(cmd, "-s", "1,lpc", NULL);
-+    virCommandAddArgList(cmd, "-s", "31,lpc", NULL);
-     return 0;
- }
- 
-@@ -425,17 +440,6 @@ bhyveBuildGraphicsArgStr(const virDomainDef *def,
-             goto error;
-         }
- 
--        if (graphics->data.vnc.auth.passwd) {
--            virReportError(VIR_ERR_CONFIG_UNSUPPORTED, "%s",
--                           _("vnc password auth not supported"));
--            goto error;
--        } else {
--             /* Bhyve doesn't support VNC Auth yet, so print a warning about
--              * unauthenticated VNC sessions */
--             VIR_WARN("%s", _("Security warning: currently VNC auth is not"
--                              " supported."));
--        }
--
-         if (glisten->address) {
-             escapeAddr = strchr(glisten->address, ':') != NULL;
-             if (escapeAddr)
-@@ -457,6 +461,9 @@ bhyveBuildGraphicsArgStr(const virDomainDef *def,
-         }
- 
-         virBufferAsprintf(&opt, ":%d", graphics->data.vnc.port);
-+        if (graphics->data.vnc.auth.passwd) {
-+            virBufferAsprintf(&opt, ",password=%s", graphics->data.vnc.auth.passwd);
-+        }
-         break;
-     case VIR_DOMAIN_GRAPHICS_LISTEN_TYPE_SOCKET:
-     case VIR_DOMAIN_GRAPHICS_LISTEN_TYPE_NONE:
-@@ -582,7 +589,6 @@ virBhyveProcessBuildBhyveCmd(bhyveConnPtr driver, virD
+@@ -708,7 +717,6 @@ virBhyveProcessBuildBhyveCmd(bhyveConnPtr driver, virD
       * since it forces the guest to exit when it spins on a lock acquisition.
       */
      virCommandAddArg(cmd, "-H"); /* vmexit from guest on hlt */
