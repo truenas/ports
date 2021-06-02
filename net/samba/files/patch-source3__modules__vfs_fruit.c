049c8510c3fdc9a996339fe21e988bec2e0f470b
Andrew Walker <awalker@ixsystems.com>
2021-06-02 04:48:32 -0400

    net/samba - fix vfs_fruit patch

--- net/samba/files/patch-source3__modules__vfs_fruit.c
+++ net/samba/files/patch-source3__modules__vfs_fruit.c
@@ -1,5 +1,5 @@
---- work/samba-4.12.15/source3/modules/vfs_fruit.c.orig	2021-01-14 03:29:57.000000000 -0500
-+++ work/samba-4.12.15/source3/modules/vfs_fruit.c	2021-05-19 10:01:25.531891000 -0400
+--- source3/modules/vfs_fruit.c.orig	2021-01-14 03:29:57.000000000 -0500
++++ source3/modules/vfs_fruit.c	2021-06-02 04:46:25.426396000 -0400
 @@ -892,6 +892,7 @@ static bool readdir_attr_meta_finderi_stream(
  	if (stream_name == NULL) {
  		return false;
@@ -40,16 +40,15 @@
  
  	rc = fruit_unlink_rsrc(handle, dirfsp, rsrc_smb_fname, true);
  	if ((rc != 0) && (errno != ENOENT)) {
-@@ -2034,6 +2046,8 @@ static int fruit_unlink_internal(vfs_handle_struct *ha
+@@ -2034,6 +2046,7 @@ static int fruit_unlink_internal(vfs_handle_struct *ha
  	}
  	TALLOC_FREE(rsrc_smb_fname);
  
 +out:
-+
  	return SMB_VFS_NEXT_UNLINKAT(handle,
  			dirfsp,
  			smb_fname,
-@@ -2134,6 +2148,7 @@ static int fruit_rmdir_internal(struct vfs_handle_stru
+@@ -2134,6 +2147,7 @@ static int fruit_rmdir_internal(struct vfs_handle_stru
  			DBG_ERR("synthetic_smb_fname failed\n");
  			return -1;
  		}
@@ -57,7 +56,7 @@
  
  		/*
  		 * Check whether it's a valid AppleDouble file, if
-@@ -2201,7 +2216,14 @@ static ssize_t fruit_pread_meta_stream(vfs_handle_stru
+@@ -2201,7 +2215,14 @@ static ssize_t fruit_pread_meta_stream(vfs_handle_stru
  	}
  
  	nread = SMB_VFS_NEXT_PREAD(handle, fsp, data, n, offset);
@@ -73,7 +72,7 @@
  		return nread;
  	}
  
-@@ -3446,6 +3468,7 @@ static NTSTATUS delete_invalid_meta_stream(
+@@ -3446,6 +3467,7 @@ static NTSTATUS delete_invalid_meta_stream(
  	if (sname == NULL) {
  		return NT_STATUS_NO_MEMORY;
  	}
@@ -81,7 +80,7 @@
  
  	ret = SMB_VFS_NEXT_UNLINKAT(handle,
  			handle->conn->cwd_fsp,
-@@ -4599,6 +4622,7 @@ static void fruit_offload_write_done(struct tevent_req
+@@ -4599,6 +4621,7 @@ static void fruit_offload_write_done(struct tevent_req
  		if (tevent_req_nomem(src_fname_tmp, req)) {
  			return;
  		}
@@ -89,7 +88,7 @@
  
  		if (is_ntfs_default_stream_smb_fname(src_fname_tmp)) {
  			TALLOC_FREE(src_fname_tmp);
-@@ -4615,6 +4639,7 @@ static void fruit_offload_write_done(struct tevent_req
+@@ -4615,6 +4638,7 @@ static void fruit_offload_write_done(struct tevent_req
  			TALLOC_FREE(src_fname_tmp);
  			return;
  		}
