44eee75b8b35fd10625717cc80ddf7ed8c8ad526
Ameer Hamza <ahamza@ixsystems.com>
2024-03-01 17:23:47 +0500

    Workaround for 13.3 failing ports

--- net-mgmt/collectd5/files/patch-version-gen.sh
+++ net-mgmt/collectd5/files/patch-version-gen.sh
@@ -1,8 +1,8 @@
---- version-gen.sh.orig	2019-06-13 09:32:35 UTC
+--- version-gen.sh.orig	2020-09-03 11:26:25 UTC
 +++ version-gen.sh
 @@ -2,10 +2,6 @@
  
- DEFAULT_VERSION="5.11.0.git"
+ DEFAULT_VERSION="5.12.0.git"
  
 -if [ -d .git ]; then
 -	VERSION="`git describe --dirty=+ --abbrev=7 2> /dev/null | sed -e '/^collectd-/!d' -e 's///' -e 'y/-/./'`"
